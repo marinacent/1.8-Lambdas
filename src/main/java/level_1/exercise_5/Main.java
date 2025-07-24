3c10b64e4897359830cdb7f3c1bf68323a6ad2b2
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-24 12:15:45 +0200

    feat: instantiate functional interface

---
+++ src/main/java/level_1/exercise_5/Main.java
@@ -0,0 +1,9 @@
+package level_1.exercise_5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PiValue pi = () -> 3.1415;
+        System.out.println(pi.getPiValue());
+    }
+}
