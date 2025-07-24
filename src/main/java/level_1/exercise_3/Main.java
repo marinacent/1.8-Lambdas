c1f7ff52575b3cae585c06dca2a788efa8ac6e00
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-24 11:26:16 +0200

    feat: print months from main

---
+++ src/main/java/level_1/exercise_3/Main.java
@@ -0,0 +1,8 @@
+package level_1.exercise_3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Months.printMonths();
+    }
+}
--- src/main/java/level_1/exercise_3/Months.java
+++ src/main/java/level_1/exercise_3/Months.java
@@ -3,7 +3,7 @@ package level_1.exercise_3;
 import java.util.Arrays;
 import java.util.List;
 
-public class PrintMonths {
+public class Months {
     private static final List<String> MONTHS = Arrays.asList("January", "February", "March", "April",
             "May", "June", "July", "August", "September", "October", "November", "December");
 
