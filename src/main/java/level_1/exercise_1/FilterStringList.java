f33ed4c91dea3f7b39be30127af1447a93d41cc6
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-24 11:19:24 +0200

    refactor: reorganize classes and printing

--- src/main/java/level_1/exercise_1/FilterStringList.java
+++ src/main/java/level_1/exercise_1/FilterStringList.java
@@ -3,7 +3,7 @@ package level_1.exercise_1;
 import java.util.List;
 import java.util.Locale;
 
-public class WithO {
+public class FilterStringList {
 
     public static List<String> getElementsWithO(List<String> list) {
         return list.stream()
@@ -16,5 +16,14 @@ public class WithO {
         System.out.println();
     }
 
+    public static List<String> getElementsWithOLongerThan5(List<String> list) {
+        return FilterStringList.getElementsWithO(list).stream()
+                .filter((a) -> a.length() > 5)
+                .toList();
+    }
 
+    public static void printElementsWithOLongerThan5(List<String> list) {
+        getElementsWithOLongerThan5(list).forEach(System.out::println);
+        System.out.println();
+    }
 }
--- src/main/java/level_1/exercise_1/Main.java
+++ src/main/java/level_1/exercise_1/Main.java
@@ -2,6 +2,7 @@ package level_1.exercise_1;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Filter;
 
 public class Main {
 
@@ -9,7 +10,12 @@ public class Main {
         List<String> nameList = Arrays.asList("Sophia", "Joan", "Neus", "Ariadna", "Miquel", "Josep");
         List<String> cityList = Arrays.asList("Oslo", "Madrid", "Barcelona", "Rome", "Riga", "Odense");
 
-        WithO.printElementsWithO(nameList);
-        WithO.printElementsWithO(cityList);
+        System.out.println("---- EXERCISE 1 ----");
+        FilterStringList.printElementsWithO(nameList);
+        FilterStringList.printElementsWithO(cityList);
+
+        System.out.println("---- EXERCISE 2 ----");
+        FilterStringList.printElementsWithOLongerThan5(nameList);
+        FilterStringList.printElementsWithOLongerThan5(cityList);
     }
 }
