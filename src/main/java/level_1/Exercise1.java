fdff4cb2b2c470c763ccbd0ada5bc00f536c32d2
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-24 10:49:57 +0200

    feat: print filtered list

--- src/main/java/level_1/Exercise1.java
+++ src/main/java/level_1/Exercise1.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
 
 public class Exercise1 {
     private static final List<String> NAME_LIST = new ArrayList<>();
@@ -16,9 +15,22 @@ public class Exercise1 {
     }
 
     public static List<String> getElementsWithO(List<String> list) {
-
         return list.stream()
                 .filter((a) -> a.toLowerCase(Locale.ROOT).contains("o"))
                 .toList();
     }
+
+    public static void printElementsWithO(List<String> list) {
+        for (String str : getElementsWithO(list)) {
+            System.out.println(str);
+        }
+        System.out.println();
+    }
+
+    public static void exercise1() {
+        printElementsWithO(NAME_LIST);
+        printElementsWithO(CITY_LIST);
+    }
+
+
 }
