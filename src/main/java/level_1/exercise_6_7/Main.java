e8e0a84c9fe726ccdda039ed2845913d34b0d35d
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-24 12:40:43 +0200

    feat: create functional interface with reverse method

--- src/main/java/level_1/exercise_6_7/Main.java
+++ src/main/java/level_1/exercise_6_7/Main.java
@@ -1,4 +1,4 @@
-package level_1.exercise_6;
+package level_1.exercise_6_7;
 
 import java.util.Arrays;
 import java.util.List;
---
+++ src/main/java/level_1/exercise_8/Reverser.java
@@ -0,0 +1,6 @@
+package level_1.exercise_8;
+
+@FunctionalInterface
+public interface Reverser {
+    String reverse(String str);
+}
