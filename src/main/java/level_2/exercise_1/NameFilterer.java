ebbe48d3d02f8e67aea34719e4498bcd23076840
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-25 16:21:17 +0200

    perf: check name length first

--- src/main/java/level_2/exercise_1/NameFilterer.java
+++ src/main/java/level_2/exercise_1/NameFilterer.java
@@ -7,7 +7,7 @@ public class NameFilterer {
     public static List<String> get3LetterNamesWithInitialA(List<String> names) {
         if (names == null) { return List.of(); }
         return names.stream()
-                .filter((name) -> name.charAt(0) == 'A' && name.length() == 3)
+                .filter((name) -> name.length() == 3 && name.charAt(0) == 'A')
                 .toList();
     }
 
