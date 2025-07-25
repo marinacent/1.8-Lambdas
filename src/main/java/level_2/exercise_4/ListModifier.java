1724539e5f168b40cf0989d569018541b54f3ac5
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-25 15:56:01 +0200

    feat: return elements that contain e first

--- src/main/java/level_2/exercise_4/ListModifier.java
+++ src/main/java/level_2/exercise_4/ListModifier.java
@@ -1,12 +1,17 @@
 package level_2.exercise_4;
 
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
 public class ListModifier {
 
     public static List<String> modifyList(List<String> list) {
         return list.stream()
                 .sorted(String::compareToIgnoreCase)
-                .toList();
+                .sorted(Comparator.comparing((a) -> !a.toLowerCase(Locale.ROOT).contains("e")))
+                .collect(Collectors.toList());
     }
 }
